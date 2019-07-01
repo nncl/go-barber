729bc9ec2a5c47ec7923fbee754eb0a4dbdc0ec5
Cauê Almeida <nncl@live.com>
2019-07-01 20:45:12 -0300

    Modeling File and associating it with Users

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,11 @@ class User extends Model {
     return this;
   }
 
+  // Create and associate the new field avatar_id
+  static associate(models) {
+    this.belongsTo(models.File, {foreignKey: 'avatar_id'});
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
