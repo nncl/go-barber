c510ed69fd87a08283220b6c8d2ffa281e069487
= <nncl@live.com>
2019-07-09 18:04:43 -0300

    Adding validations about dates: only appointments per hour, which is not with the same provider and also obviously not at the  same time

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import {startOfHour, parseISO, isBefore} from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -29,6 +30,32 @@ class AppointmentController {
       return res.status(401).json({
         error: 'You can only book an appointment with providers',
       });
+    }
+
+    /**
+     * Check for past dates
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({error: 'Past dates are not allowed'});
+    }
+
+    /**
+     * Check for availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({error: 'Date is not available'});
     }
 
     const appointment = await Appointment.create({
