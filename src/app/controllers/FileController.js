5390a38abfe530802cb0e56d20722df6c28ad53c
= <nncl@live.com>
2019-07-09 16:23:56 -0300

    Adding validation on upload avatar file

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -2,6 +2,7 @@ import File from '../models/File';
 
 class FileController {
   async store(req, res) {
+    if (!req.file) return res.status(422).json({error: 'Validation failed'});
     const {originalname: name, filename: path} = req.file;
 
     const file = await File.create({name, path});
