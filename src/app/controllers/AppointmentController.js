283114997415b3b34120f89e25166ed078cebe80
Cauê Almeida <nncl@live.com>
2019-07-10 20:33:26 -0300

    Adding pagination on appointment list

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,14 +2,18 @@ import * as Yup from 'yup';
 import {startOfHour, parseISO, isBefore} from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
-import File from "../models/File";
+import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const {page = 1} = req.query;
+
     const appointments = await Appointment.findAll({
       where: {user_id: req.userId, canceled_at: null},
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
