d27fa5313cfe37db3fe47626a9e46fbce6b0cc23
Cauê Almeida <nncl@live.com>
2019-07-11 20:01:10 -0300

    Update quotes

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -2,7 +2,7 @@ import {startOfDay, endOfDay, parseISO} from 'date-fns';
 import {Op} from 'sequelize';
 
 import User from '../models/User';
-import Appointment from "../models/Appointment";
+import Appointment from '../models/Appointment';
 
 class ScheduleController {
   async index(req, res) {
