e0976f41acfcb64a9da6b197e4b6c92a0bc97437
Cauê Almeida <nncl@live.com>
2019-07-11 20:48:19 -0300

    Inserting provider's appointment notification into mongo

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import {startOfHour, parseISO, isBefore} from 'date-fns';
+import {startOfHour, parseISO, isBefore, format} from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -91,6 +93,22 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {locale: pt}
+    );
+
+    const ex = await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
