538b772357b6d34a36ba7234275879e634b8a0b2
Cauê Almeida <nncl@live.com>
2019-07-12 19:56:50 -0300

    Route to cancel an appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import {startOfHour, parseISO, isBefore, format} from 'date-fns';
+import {startOfHour, parseISO, isBefore, format, subHours} from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -110,6 +110,31 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    // Check if it's their own appointment and not other's
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'You can only cancel your own appointments',
+      });
+    }
+
+    // Users can only cancel with 2 hours max before the appointment
+    const dateWithSub = subHours(appointment.date, 2);
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel an appointment 2 hours in advance',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
