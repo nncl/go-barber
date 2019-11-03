ceedf11e2c84cb90dc282f836f0ed2bf3a906ab4
= <nncl@live.com>
2019-11-03 17:06:00 -0300

    - Add CORS - Adding fields to match the admin app

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -72,12 +73,22 @@ class UserController {
       return res.status(401).json({error: 'Password does not match'});
     }
 
-    const {id, name, provider} = await user.update(req.body);
+    await user.update(req.body);
+
+    const {id, name, avatar} = await User.findByPk(req.user.id, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
-      provider,
+      avatar,
     });
   }
 }
